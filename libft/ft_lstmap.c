0dfc2e8d85f65791dd2605fbfe40c55531e21527
Hyunwoo Cho <hcho@c1r2s1.42seoul.kr>
2020-12-24 18:28:59 +0900

    aa

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: hcho <hcho@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/24 08:21:17 by hcho              #+#    #+#             */
-/*   Updated: 2020/12/24 18:22:46 by hcho             ###   ########.fr       */
+/*   Updated: 2020/12/24 18:27:41 by hcho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if ((new_list = ft_lstnew(f(lst->content))) == 0)
 		return (0);
 	curr = new_list;
-	lst = lst -> next;
+	lst = lst->next;
 	while (lst)
 	{
 		if ((new_elem = ft_lstnew(f(lst->content))) == 0)
