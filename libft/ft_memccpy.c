92beb3de23f8d6e63cd902630b287efac989dc16
Hyunwoo Cho <hcho@c1r2s6.42seoul.kr>
2020-12-27 11:51:49 +0900

    aa

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: hcho <hcho@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 11:55:12 by hcho              #+#    #+#             */
-/*   Updated: 2020/12/24 14:50:05 by hcho             ###   ########.fr       */
+/*   Updated: 2020/12/27 10:51:00 by hcho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	size_t i;
 
-	i = -1;
-	while (++i < n && *((unsigned char *)src + i))
+	i = 0;
+	while (i < n)
 	{
 		if (*((unsigned char *)src + i) == (unsigned char)c)
 		{
@@ -25,8 +25,7 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 			return (dest + i + 1);
 		}
 		*((unsigned char *)dest + i) = *((unsigned char *)src + i);
+		i++;
 	}
-	if (!*((unsigned char *)src + i))
-		*((unsigned char *)dest + i) = 0;
 	return (0);
 }
